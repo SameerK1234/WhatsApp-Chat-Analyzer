d3bb8fc8a76108530c1aa1a6d880974a9fbda1c5
SameerK1234 <145307730+SameerK1234@users.noreply.github.com>
2024-11-12 15:31:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import seaborn as sns
+import os
 import nltk
-nltk.data.path.clear()
-nltk.download('punkt',force=True)
+nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
+nltk.data.path.append(nltk_data_path)
+nltk.download('punkt', download_dir=nltk_data_path)
+# nltk.data.path.clear()
+# nltk.download('punkt',force=True)
 nltk.download("stopwords")
 
 
