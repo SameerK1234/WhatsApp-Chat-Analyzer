52abd12e093dc64f880684dd63f14999322b75c0
SameerK1234 <145307730+SameerK1234@users.noreply.github.com>
2024-11-17 00:31:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
 nltk.data.path.append(nltk_data_path)
 nltk.download('punkt', download_dir=nltk_data_path)
 nltk.data.path.clear()
-nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download("stopwords")
 
 
